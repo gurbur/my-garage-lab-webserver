00759e9f318eee5ca336c193513e6a8405e3bdac
gurbur <gurbur331@ajou.ac.kr>
2025-07-26 19:56:01 +0900

    Feat: server module

---
+++ src/server.c
@@ -0,0 +1,45 @@
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include "server.h"
+#include "logger.h"
+
+int init_server(server_config *config) {
+	int listen_fd;
+	struct sockaddr_in server_addr;
+
+	if ((listen_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		log_message("ERROR: socket() failed");
+		return -1;
+	}
+
+	int opt = 1;
+	if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+		log_message("ERROR: setsockopt(SO_REUSEADDR) failed");
+		close(listen_fd);
+		return -1;
+	}
+
+	memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_addr.sin_port = htons(config->port);
+
+	if (bind(listen_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+		log_message("ERROR: bind() failed on port %d", config->port);
+		close(listen_fd);
+		return -1;
+	}
+
+	if (listen(listen_fd, SOMAXCONN) < 0) {
+		log_message("ERROR: listen() failed");
+		close(listen_fd);
+		return -1;
+	}
+
+	log_message("Server listening on port %d", config->port);
+	return listen_fd;
+}
+
---
+++ src/server.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "config.h"
+
+int init_server(server_config* config);
+
